0e1d938b76ca3785d03f7fda2fd9ad6a96e8fcde
Sethu Vignesh <sethu@circles.asia>
2020-08-19 11:59:07 +0800

    exception handling

---
+++ src/DivideByZeroLib.java
@@ -0,0 +1,8 @@
+public class DivideByZeroLib {
+    public static void main(String[] args) {
+
+    }
+    private static void dividBy0() throws Exception {
+        int i = 1 / 0;
+    }
+}
--- src/LearnException.java
+++ src/LearnException.java
@@ -8,21 +8,54 @@ public class LearnException {
     //https://docs.oracle.com/javase/tutorial/essential/exceptions/QandE/questions.html
 
     public static void main(String[] args) {
-        dividebyZero();
-        try {
-            // int i = 1 / 0;
-            dividebyZero();
-            System.out.println("try block");
-            // int i= 1/0;
-        } catch (Exception e) {
-            System.out.println("catch block");
-        } finally {
-            System.out.println("finally block");
+
+//        try {
+//            int i = 1 / 0;
+//        } catch (NullPointerException e) {
+////            e.printStackTrace();
+//            //user dialog null
+//        } catch (ArithmeticException e) {
+//            //1-8 select
+//        } catch (Exception e) {
+//
+//        }
+
+
+//        try {
+//            System.out.println("try 1");
+//            int i = 1 / 0;
+//            System.out.println("try 2");
+//        } catch (Exception e) {
+//            System.out.println("exception");
+//
+//        } finally {
+//            System.out.println("finally");
+//        }
+
+//        Thread.sleep(5000);
+
+//        try {
+//            dividBy0();
+//        }
+//        catch (Exception e) {
+//            e.printStackTrace();
+//            System.out.println("Please try agian");
+//            System.out.println("thirumba try pannunga");
+//        }
+
+        try{
+
+        }catch (MyException e){
+            e.printStackTrace();
         }
+
+
     }
 
-    private static int dividebyZero() throws ArithmeticException {
+
+    private static void dividBy0() throws Exception {
         int i = 1 / 0;
-        return i;
     }
+
+
 }
